68a60d60d169969497b76414b9df1d7c75de729f
PGalicia <galicia.patrick@gmail.com>
2018-05-14 14:26:08 -0700

    Homepage
    - Convert it to PHP

--- home.php
+++ home.php
@@ -6,20 +6,22 @@
 		<div id="navbar">
 			
 			<?php
-				// List menu items horizontally
+				// Description: List menu items horizontally
 				// Parameter: $subname: The name of the button
 				//			  $link: The link of the button
 				function menuButton($subname, $link) {
+					$style = (strlen($subname) >= 10) ? "1.2em" : "1.6em";
 					echo "<a href='" . $link . "'>";
 					echo "<div class='subcategory'>";
-					echo "<p>" . $subname . "</p>";
+					echo "<p style='padding-top: " . $style . ";'>" . $subname . "</p>";
 					echo "</div>";
 					echo "</a>";
 				}
 
 				echo "<nav>";
 
-				$subcategory = array("SEXUAL HEALTH", "MENTAL HEALTH", "PHYSICAL ACTIVITY", "ALCOHOL ACTIVITY", "NUTRITION ACTIVITY");
+				// Note: Change when database access is figured out
+				$subcategory = array("SEXUAL HEALTH", "MENTAL HEALTH", "PHYSICAL ACTIVITY", "ALCOHOL ACTIVITY", "NUTRITION");
 				foreach($subcategory as $sub) {
 					menuButton($sub, "");
 				}
@@ -30,24 +32,32 @@
 
 		</div>
 		<div id="search">
-			<!-- PHP conversion: Ensure that the default on the SPAN is random -->
-			<!-- Dropdown Menu -->
-			<!-- Reference -->
-			<p>I'm Looking For: </p>
-			<dl class="dropdown">
-				<dt><a><span style="font-weight: normal;">Dropdown n°1</span></a></dt>
-				<dd>
-					<ul>
-						<li><a class="selected">Dropdown n°1</a></li>
-						<li><a>Option n°1</a></li>
-						<li><a>Option n°2</a></li>
-						<li><a>Option n°3</a></li>
-					</ul>
-				</dd>
-			</dl>
-			<button id="check">Press Me</button>
+			
+			<?php
+				$dropdown_choices = array("Counseling Services" , "Fitness Groups" , "Anxiety Resources");
+				// Description: Create the dropdown menu
+				// Parameter: $choice: list of choices in dropdown menu
+				function dropdown($choices) {
+					// Choose a random element from the list to be default
+					$i = array_rand($choices);
+					echo "<dt><a><span style='font-weight: normal; font-size: 1.5em;'>" . $choices[$i] . "</span></a></dt>";
+					echo "<dd><ul>";
+					echo "<li><a class='selected'>" . $choices[$i] . "</a></li>";
+					
+					// Print the rest in the dropdown menu
+					unset($choices[$i]);
+					foreach($choices as $choice) {
+						echo "<li><a>" . $choice . "</a></li>";
+					}
+					echo "</ul></dd>";					
+				}
 
-			<!-- ------------- -->
+				echo "<p>I'M LOOKING FOR </p>";
+				echo "<dl class='dropdown'>";
+				dropdown($dropdown_choices);
+				echo "</dl>";
+				echo "<button id='submit_search'>Search</button>";
+			?>
 
 		</div>
 	</div>
@@ -57,8 +67,8 @@
 	
 	$(document).ready(function() {
 
-		// Check what's chosen
-		$("#check").click(function() {
+		// Check what's selected
+		$("#submit_search").click(function() {
 			var selected = $("dt a span").html();
 			alert(selected);
 		});
--- test.php
+++ test.php
@@ -1,64 +1,6 @@
-<link rel="stylesheet" href="css/dropdown.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<!-- <h1>Dropy</h1>
-<h2>A Simple SCSS & jQuery dropdown</h2>
-
-<main>
-  <p><a href="https://codepen.io/Tombek/pen/OPvpLe" target="_blank">I've made a new version of Dropy !</a></p>
-</main> -->
-
-
-<dl class="dropdown">
-	<dt><a><span style="font-weight: normal;">Dropdown n°1</span></a></dt>
-	<dd>
-		<ul>
-			<li><a class="selected">Dropdown n°1</a></li>
-			<li><a>Option n°1</a></li>
-			<li><a>Option n°2</a></li>
-			<li><a>Option n°3</a></li>
-		</ul>
-	</dd>
-</dl>
-
-<button id="check">Press Me</button>
-
-<!-- <dl class="dropdown">
-	<dt><a><span>Dropdown n°2</span></a></dt>
-		<dd>
-			<ul>
-				<li><a class="default">Dropdown n°2</a></li>
-				<li><a>Option n°1</a></li>
-				<li><a>Option n°2</a></li>
-				<li><a>Option n°3</a></li>
-				<li><a>Option n°4</a></li>
-				<li><a>Option n°5</a></li>
-				<li><a>Option n°6</a></li>
-			</ul>
-		</dd>
-</dl>
-
-<dl class="dropdown">
-	<dt><a><span>Dropdown n°3</span></a></dt>
-		<dd>
-			<ul>
-				<li><a class="default">Dropdown n°3</a></li>
-				<li><a>Option n°1</a></li>
-				<li><a>Option n°2</a></li>
-			</ul>
-		</dd>
-</dl> -->
-<script src="js/dropdown.js"></script>
-<script type="text/javascript">
+<?php
 	
-	$(document).ready(function() {
+	// Access text file
+	echo file_get_contents("http://centaurus-2.ics.uci.edu:8926/hcampus/database.txt");
 
-		// Check what's chosen
-		$("#check").click(function() {
-			var selected = $("dt a span").html();
-			alert(selected);
-		});
-
-
-	});
-</script>
+?>
