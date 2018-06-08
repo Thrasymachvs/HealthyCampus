389966aa9d901ededb53565a87aee1d42f8d7df0
Thrasymachvs <christopherlionheartchristian@gmail.com>
2018-06-08 12:19:42 -0700

    Category commented

--- Category.php
+++ Category.php
@@ -6,7 +6,7 @@
 	// Access the chosen group
 	$group = $_GET["group"];
 
-
+		//sets the path for the image to be used on the page. In this case, the name of the image must share the same name as the $group variable, and it must be placed in the img/bg folder. Additionally, it must be a .png file. 
 		$bg_image = "img/bg/" . $group . ".png";
 	
 	// Access both 'category' and 'organization' data
@@ -23,16 +23,21 @@
 
 	// Access the information that holds the information of the specified category
 	$working_group = $category_data[$key]["category"];
+	//Hotfixes for a few working groups that wouldn't format right
 	if ($working_group == "alcohol"){
 		$title = "Alcohol & Other Drugs";
 	}
+	//Hotfix for working group that doesn't format right. 
 	else if ($working_group == "nutrition"){
 		$title = "Nutritional";
 	}
+	//General case: Just keep the $group value as the name
 	else{
 		$title = ucfirst(strval($working_group));
 	}
+	//Get the description from the database 
 	$working_description = $category_data[$key]["description"];
+	//Pull the hotlines from the database
 	$hotlines= $category_data[$key]["hotlines"];
 	
 	// Access Faculty' resrouces
@@ -53,8 +58,11 @@
 
 <!-- Modal link style -->
 <!-- Need to move to CSS after the update! -->
+
 <style type="text/css">
 body{
+	
+
 	background-image: url(<?php echo $bg_image?>);
 }
 	.myBtn {
@@ -72,7 +80,9 @@ body{
 		<div class = "card">
 			<div class = "container">
 				<?php 
+				//Print the header with the working group in the title 
 					echo "<h1 class = 'header'>". $title . " Health</h1>"; 
+					//Iterate through the working description's sections and print out the data from the database. 
 					for ($i = 0; $i < count($working_description); $i++){
 						echo "<p class = 'text_body'>" . $working_description[$i] . "</p>";
 					}
@@ -87,6 +97,7 @@ body{
 				<h3 class = 'header'>Faculty Resource Links</h3>
 				<ul>
 					<?php
+					//Iterate through all of the resources for faculty members and print it out with the appropriate links to the modals
 						foreach($faculty_resources as $key){
 							echo "<li class = 'text_setting'><a class=\"myBtn\" onclick=\"openModal('" . $key["organization"] . "')\">" . $key["organization"] . "</a></li>";
 						} 
@@ -101,7 +112,9 @@ body{
 			<div class = "container">
 				<h3 class = 'header'>Student Resource Links</h3>
 				<ul>
+
 					<?php
+					//Iterate through all of the resources for students and print the resources out with appropriate links to the modals 
 						foreach($student_resources as $key){
 							echo "<li class = 'text_setting'><a class=\"myBtn\" onclick=\"openModal('" . $key["organization"] . "')\">" . $key["organization"] . "</a></li>";
 						} 
@@ -120,6 +133,7 @@ body{
 					<h2 class = 'header'> National Hotlines </h2>
 					<ul>
 						<?php
+							//Print out each Hotline name and the appropriate phone number., 
 							foreach (array_keys($hotlines) as $key){
 								echo "<li class ='header'>" . $key . "  : <br> <p class = 'number_text'> " . $hotlines[$key] .  "</p></li>";
 							}
